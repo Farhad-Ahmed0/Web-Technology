b83861cb07f09df9afdc1a8bdf953ea9fd077f06
Farhad Ahmed <62322908+Farhad-Ahmed0@users.noreply.github.com>
2022-03-12 01:42:20 +0600

    Lab task 4
    Lab task 4

---
+++ Lab task 4/PublicHome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fooide </title>
+
+</head>
+<body>
+    <?php include 'header.php'; ?>
+<span style = "display:inline-block; width:100%;text-align:left; height: 30%; padding:10px">
+<?php
+echo "<h3>Welcome to FOODIE</h3></p>";
+echo "<h3>Foodie is an e-commerce food delivery website. We all know that food delivery websites have become very popular in all countries. With this website, we can order the food we need from anywhere in a very short time, and we can get the food on time. This will protect us from wasting our efforts and time. As a result, it's easy to say that this website is essential for us right now.</h3></p>";
+?>
+</span> 
+<?php include 'footer.php';?>
+</body>
+</html>
